e62fe2660deb10e99d8442ac7748dec316017997
Natali <nbyka@mail.ru>
2023-04-08 21:30:55 +0300

    Summa

---
+++ Task27_Summa/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Read()
+{
+    System.Console.WriteLine("Введите число: ");
+    int Num = int.Parse(Console.ReadLine());
+    return Num;
+}
+int Num = Read();
+
+void Task()
+{
+    int summa = 0;
+    while(Num > 0)
+    {
+        int remain = Num % 10;
+        summa += remain;
+        Num = Num / 10;
+    }
+    Console.WriteLine(summa);
+}
+Task();
